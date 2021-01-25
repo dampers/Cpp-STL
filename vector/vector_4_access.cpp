0c47dba0e23917ba2114ab121ae9d723d5bb66ae
dampers <houma757@gmail.com>
2021-01-25 10:14:23 +0900

    edit how to access element in vector

--- vector/vector_4_access.cpp
+++ vector/vector_4_access.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main()
 {
 	vector<int> v = {10, 20, 30, 40};
 	for(size_t i=0;i<v.size();i++)
-		cout << v[i] << ' ';
-	cout << endl;
+		cout << v[i] <<' ';
+	cout<<endl;
 
-	v[2] = 100;
+	v.at(2) = 100;
+	v[3] = 44;
 
 	for(size_t i=0;i<v.size();i++)
-		cout << v[i] << ' ';
-	cout << endl;
-	
+		cout<< v.at(i) << ' ';
+	cout<<endl;
 	return 0;
 }
-
