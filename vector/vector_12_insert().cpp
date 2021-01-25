0ab6e05451b4d3bd7351c563143931aae1687c7a
dampers <houma757@gmail.com>
2021-01-25 10:01:08 +0900

    add vector member function insert()

---
+++ vector/vector_12_insert().cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print_container(const vector<int>& v)
+{
+	for(size_t i=0;i<v.size();i++) cout<<v[i]<<' ';
+	cout<<endl;
+}
+
+int main()
+{
+	vector<int> v = {1, 2, 3, 4};
+	vector<int>::iterator it = v.begin();
+	cout<<"Before insert : ";
+	print_container(v);
+
+	//v.insert(iter, value) inserts value before iterator pos.
+	// returns iterator pointing to the inserted value.
+	//it = v.insert(it, 5);
+	v.insert(it, 5);
+	v.insert(v.begin()+3, 5);
+	cout<<"After insert : ";
+	print_container(v);
+	return 0;
+}
